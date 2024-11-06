7849517f50e74fa355a87f2990c57c9fdcecf5a8
Seongju38 <ljg013@naver.com>
2024-11-06 14:50:54 +0900

    main.tsx 오류 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App"; // TypeScript는 파일의 확장자를 자동으로 인식
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
