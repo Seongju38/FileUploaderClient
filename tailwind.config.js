c56eaed561c9d6658acd9443381c358ad1dd772c
Seongju38 <ljg013@naver.com>
2024-11-06 15:34:02 +0900

    tailwind 설정 파일 수정 - ant design과의 스타일 충돌 방지

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind 적용 범위
+    "node_modules/antd/es/**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false, // Ant Design의 기본 스타일과 충돌 방지 -> Ant Design의 클래스에 우선순위 부여
+  },
 }
 
